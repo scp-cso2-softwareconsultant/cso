37af17ad09d88ee4430b756142086f9d381d4b1c
zalven88 <zalvendayao888@gmail.com>
2021-11-19 11:40:37 +0800

    duplication

--- app/Http/Controllers/FinanceTrackerController.php
+++ app/Http/Controllers/FinanceTrackerController.php
@@ -39,35 +39,70 @@ class FinanceTrackerController extends Controller
             ]);
     }
 
+    public function getFinance($given_cso){
+        $finance_tracker = DB::table('finance')
+            ->where('finance_name',$given_cso)
+            ->whereRaw(DB::raw("deleted_at IS NULL"))
+            ->get()
+            ->first();
+        return $finance_tracker ;
+    }
+
+
     public function saveFinanceTracker(Request $request){
         $raw_data = json_decode($request['data']);
         $form_mode = $request['form_mode'];
         $success = false;
+
+
+
+
         $user = Auth::user();
         $user_name = $user->firstname . ' '. $user->lastname;
+
+        $finance_name = $raw_data->finance_name;
+
         $finance_budget = $raw_data->finance_budget != '' ? $raw_data->finance_budget : 0;
+
         $finance_tranche1 = $raw_data->finance_tranche1 != '' ? $raw_data->finance_tranche1 : 0;
         $finance_tranche2 = $raw_data->finance_tranche2 != '' ? $raw_data->finance_tranche2 : 0;
         $finance_tranche3 = $raw_data->finance_tranche3 != '' ? $raw_data->finance_tranche3 : 0;
         $finance_tranche4 = $raw_data->finance_tranche4 != '' ? $raw_data->finance_tranche4 : 0;
-        $finance_retention = $raw_data->finance_retention != ''? $raw_data->finance_retention: 0;
 
         $finance_tranche1_date = ( $raw_data->finance_tranche1_date != '') ? date('Y-m-d',strtotime( $raw_data->finance_tranche1_date ) ): '';
         $finance_tranche2_date = ( $raw_data->finance_tranche2_date != '') ? date('Y-m-d',strtotime( $raw_data->finance_tranche2_date ) ): '';
         $finance_tranche3_date = ( $raw_data->finance_tranche3_date != '') ? date('Y-m-d',strtotime( $raw_data->finance_tranche3_date ) ): '';
         $finance_tranche4_date = ( $raw_data->finance_tranche4_date != '') ? date('Y-m-d',strtotime( $raw_data->finance_tranche4_date ) ): '';
 
-        
-        $finance_total = $finance_tranche1 + $finance_tranche2 + $finance_tranche3 + $finance_tranche4 ;
+        $finance_retention = $raw_data->finance_retention != ''? $raw_data->finance_retention: 0;
         $finance_actuals = $raw_data->finance_actuals != '' ? $raw_data->finance_actuals : 0;
+
+        $finance = $this->getFinance($finance_name);
+        if( $finance != null && $form_mode < 0) {
+
+            $finance_budget += $finance->finance_budget;
+            $finance_tranche1 += $finance -> finance_tranche1;
+            $finance_tranche2 += $finance -> finance_tranche2;
+            $finance_tranche3 += $finance -> finance_tranche3;
+            $finance_tranche4 += $finance -> finance_tranche4;
+            $finance_retention += $finance -> finance_retention;
+            $finance_actuals += $finance -> finance_actuals;
+
+            Log::info( $finance_budget );
+        }
+
+        $finance_total = $finance_tranche1 + $finance_tranche2 + $finance_tranche3 + $finance_tranche4 ;
         $finance_variance = $finance_total - $finance_actuals;
         $finance_burn_rate = $finance_actuals / ($finance_tranche1+$finance_tranche2+$finance_tranche3+$finance_tranche4+$finance_retention)* 100; 
 
-        $this->saveCSOLeadOrganizationIfNotExist($raw_data->finance_name);
+        $this->saveCSOLeadOrganizationIfNotExist($finance_name);
+        
+        
 
-        if($form_mode < 0) {
+
+        if($form_mode < 0 && $finance == null) {
             $insertData = DB::table('finance')->insert([
-                'finance_name' => $raw_data->finance_name,
+                'finance_name' => $finance_name,
                 'finance_budget' => $finance_budget,
                 'finance_tranche1' => $finance_tranche1,
                 'finance_tranche2' => $finance_tranche2,
@@ -89,27 +124,31 @@ class FinanceTrackerController extends Controller
             ]);
             if($insertData) $success=true;
         }else{
-            $updateData = DB::table('finance')->where('finance_id',$raw_data->finance_id)
-                ->update(array(
-                    'finance_name' => $raw_data->finance_name,
-                    'finance_budget' => $finance_budget,
-                    'finance_tranche1' => $finance_tranche1,
-                    'finance_tranche2' => $finance_tranche2,
-                    'finance_tranche3' => $finance_tranche3,
-                    'finance_tranche4' => $finance_tranche4,
-                    'finance_retention' => $finance_retention,
-                    'finance_tranche1_date' => $finance_tranche1_date,
-                    'finance_tranche2_date' => $finance_tranche2_date,
-                    'finance_tranche3_date' => $finance_tranche3_date,
-                    'finance_tranche4_date' => $finance_tranche4_date,
-                    'finance_total' => $finance_total,
-                    'finance_actuals' => $finance_actuals,
-                    'finance_variance'=> $finance_variance,
-                    'finance_burn_rate' => $finance_burn_rate,
-                    'updated_at' => date("Y-m-d h:i:s"),
-                    'updated_by' => $user_name,
-                ));
-            if($updateData) $success=true;
+            $finance_id = $finance -> finance_id;
+
+            if( $finance_id != -1 ){ 
+                $updateData = DB::table('finance')->where('finance_id',$finance_id)
+                    ->update(array(
+                        'finance_name' => $raw_data->finance_name,
+                        'finance_budget' => $finance_budget,
+                        'finance_tranche1' => $finance_tranche1,
+                        'finance_tranche2' => $finance_tranche2,
+                        'finance_tranche3' => $finance_tranche3,
+                        'finance_tranche4' => $finance_tranche4,
+                        'finance_retention' => $finance_retention,
+                        'finance_tranche1_date' => $finance_tranche1_date,
+                        'finance_tranche2_date' => $finance_tranche2_date,
+                        'finance_tranche3_date' => $finance_tranche3_date,
+                        'finance_tranche4_date' => $finance_tranche4_date,
+                        'finance_total' => $finance_total,
+                        'finance_actuals' => $finance_actuals,
+                        'finance_variance'=> $finance_variance,
+                        'finance_burn_rate' => $finance_burn_rate,
+                        'updated_at' => date("Y-m-d h:i:s"),
+                        'updated_by' => $user_name,
+                    ));
+                if($updateData) $success=true;
+            }
         }
 
         $data_arr = [
